91ca4f8616bdbc288b98e9bbe865361c7a5a69a8
Grayson Gantek <graysonleegantek@gmail.com>
2020-12-29 15:37:59 -0500

    adds maintenance page template

---
+++ maintenance-template.php
@@ -0,0 +1,19 @@
+<?php
+// Template Name: Maintenance
+/**
+ *
+ * 
+ * 
+ */
+?>
+<html>
+<style>
+  h2,
+  p {
+    font-family: sans-serif;
+    text-align: center;
+  }
+</style>
+​
+<h2 style="font-size: 45px; margin: 35px 0;">THIS SITE IS CURRENTLY UNAVAILABLE</h2>
+<p style="font-size: 22px;">IF YOU ARE THE OWNER, PLEASE CONTACT YOUR HOSTING PROVIDER FOR BILLING MATTERS.</p>
