ef2e48ded3593735a759eff4b333eb71535b254b
Grayson Gantek <graysonleegantek@gmail.com>
2021-01-25 16:16:10 -0500

    updates maintenance template

---
+++ page-templates/maintenance-template.php
@@ -0,0 +1,24 @@
+<?php
+// Template Name: Maintenance
+/**
+ *
+ * 
+ * 
+ */
+?>
+<html lang="en">
+    <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Currently UNAVAILABLE</title>
+<style>
+  h2,
+  p {
+    font-family: sans-serif;
+    text-align: center;
+  }
+</style>
+</html>
+<body>
+  <h2 style="font-size: 45px; margin: 35px 0;">THIS SITE IS CURRENTLY UNAVAILABLE</h2>
+  <p style="font-size: 22px;">IF YOU ARE THE OWNER, PLEASE CONTACT YOUR HOSTING PROVIDER FOR BILLING MATTERS.</p>
+</body>
