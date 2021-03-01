0e38f316b5a5b604dc2947ea3d0d7ebef2e939e5
Grayson Gantek <graysonleegantek@gmail.com>
2021-03-01 20:06:38 +0000

    adds container helper classes; removes global default margin and padding

--- template-parts/content/content-page.php
+++ template-parts/content/content-page.php
@@ -10,7 +10,7 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<article id="post-<?php the_ID(); ?>" <?php post_class('container'); ?>>
   <?php if (!is_front_page()) : ?>
     <header class="entry-header">
       <?php // get_template_part('template-parts/header/entry-header'); 
