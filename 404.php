c297b33b0b09d0ee5f0721a1fecf7cff6c961067
Grayson Gantek <graysonleegantek@gmail.com>
2021-07-12 10:22:13 -0400

    updates 404 page styling

--- 404.php
+++ 404.php
@@ -1,18 +1,19 @@
 <?php
+
 // Exit if accessed directly.
 defined('ABSPATH') || exit;
 
 get_header();
-$container = get_theme_mod('container_width');
+
 ?>
 
 <main class="not-found">
   <div class="not-found__container">
     <section class="not-found__section">
-      <h1>404</h1>
-      <h3>The page you are looking for wasn't found.</h3>
-      <p>
-        <a href="/">Homepage</a>
+      <h1 class="not-found__h2">404</h1>
+      <h2 class="not-found__h1">The page you are looking for wasn't found.</h2>
+      <p class="not-found__buttons">
+        <a class="not-found__button" href="<?php echo esc_url(get_home_url()); ?>">Homepage</a>
       </p>
     </section>
   </div>
