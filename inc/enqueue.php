ce73a0d9825d995121deffc8506c1986fbf1236a
Grayson Gantek <graysonleegantek@gmail.com>
2021-10-28 16:23:56 -0400

    restructures some assets for gulp

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -11,37 +11,37 @@ defined('ABSPATH') || exit;
 function pc_enqueues()
 {
   // Sitewide
-  wp_enqueue_style('main', get_stylesheet_directory_uri() . '/css/main.css');
-  wp_enqueue_script('precision', get_stylesheet_directory_uri() . '/js/main.js', array(), false, true);
+  wp_enqueue_style('main', get_stylesheet_directory_uri() . '/dist/css/main.css');
+  wp_enqueue_script('precision', get_stylesheet_directory_uri() . '/dist/js/main.js', array(), false, true);
 
   // 404 Page (Conditional)
   if (is_404()) {
-    wp_enqueue_style('not-found', get_stylesheet_directory_uri() . '/css/page-404.css');
+    wp_enqueue_style('not-found', get_stylesheet_directory_uri() . '/dist/css/page-404.css');
   }
 
   // Blog page (Conditional)
   if (is_home() && !is_front_page() || is_category() || is_tag()) {
-    wp_enqueue_style('blog', get_stylesheet_directory_uri() . '/css/page-blog.css');
+    wp_enqueue_style('blog', get_stylesheet_directory_uri() . '/dist/css/page-blog.css');
   }
 
   // Single page (Conditional)
   if (is_single()) {
-    wp_enqueue_style('single', get_stylesheet_directory_uri() . '/css/page-single.css');
+    wp_enqueue_style('single', get_stylesheet_directory_uri() . '/dist/css/page-single.css');
   }
 
   // Blocks page
   if ('page-templates/blocks.php' === get_page_template_slug()) {
-    wp_enqueue_style('blocks', get_stylesheet_directory_uri() . '/css/blocks.css');
+    wp_enqueue_style('blocks', get_stylesheet_directory_uri() . '/dist/css/blocks.css');
   }
 
   // Search page
   if (is_page_template('page-templates/searchpage.php')) {
-    wp_enqueue_style('page-search', get_stylesheet_directory_uri() . '/css/page-search.css');
+    wp_enqueue_style('page-search', get_stylesheet_directory_uri() . '/dist/css/page-search.css');
   }
 
   // Search results page 
   if (is_search()) {
-    wp_enqueue_style('page-search-results', get_stylesheet_directory_uri() . '/css/page-search-results.css');
+    wp_enqueue_style('page-search-results', get_stylesheet_directory_uri() . '/dist/css/page-search-results.css');
   }
 }
 add_action('wp_enqueue_scripts', 'pc_enqueues');
